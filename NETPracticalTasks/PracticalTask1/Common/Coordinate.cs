85edec7ce6d410fb02bd62522af85c8e227ee084
Sergey Zykin <zykin_si@directum.ru>
2023-06-13 15:43:42 +0400

    updated description of Coordinate record (added remarks)

--- NETPracticalTasks/PracticalTask1/Common/Coordinate.cs
+++ NETPracticalTasks/PracticalTask1/Common/Coordinate.cs
@@ -2,9 +2,11 @@ namespace ConsoleApp1.PracticalTask1.Common
 {
   /// <summary>
   /// Запись для координаты.
+  /// </summary>
+  /// <remarks>
   /// Выбор использования этой записи вместо System.Windows.Point обуславлевается тем,
   /// что библиотека WindowsBase недоступна для .NET 6, а также выбранная мной модель более соответствует предметной области.
-  /// </summary>
+  /// </remarks>
   /// <param name="Latitude">Широта</param>
   /// <param name="Longitude">Долгота</param>
   public record Coordinate(double Latitude, double Longitude);
